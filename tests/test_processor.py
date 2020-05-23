458f654c2ab7c143a29b39bcf2858db5f50be7b0
Miroslav Šedivý <6774676+eumiro@users.noreply.github.com>
2020-05-23 21:48:25 +0200

    Pack everything into DataFrames

--- tests/test_processor.py
+++ tests/test_processor.py
@@ -8,81 +8,77 @@ import pytest
 import pygohome.processor as processor
 
 
-def test_prepare_tracks_empty_raises() -> None:
+def test_prepare_trackpoints_empty_raises() -> None:
     """Empty list returns an empty DataFrame."""
     with pytest.raises(processor.EmptyDataError):
-        processor.prepare_tracks([])
+        processor.prepare_trackpoints([])
 
 
-def test_prepare_tracks_one_row_ok() -> None:
+def test_prepare_trackpoints_one_row_ok() -> None:
     """One record returns good data."""
     trackpoints = [(dt.datetime(2020, 5, 1, tzinfo=dt.timezone.utc), 49.00, 8.40)]
-    result = processor.prepare_tracks(trackpoints)
-    expected = {
-        "dfr": pd.DataFrame(
-            {"segment": [0], "offset": [0], "utm_x": [456114], "utm_y": [5427629]}
-        ),
-        "utm_zone": 32,
-        "utm_ch": "U",
-    }
-    pd.testing.assert_frame_equal(result.pop("dfr"), expected.pop("dfr"))
-    assert result == expected
+    result = processor.prepare_trackpoints(trackpoints)
+    expected = pd.DataFrame(
+        {
+            "utm_x": [456114],
+            "utm_y": [5427629],
+            "utm_zone": [32],
+            "utm_ch": ["U"],
+            "segment": [0],
+            "offset": [0],
+        }
+    )
+    pd.testing.assert_frame_equal(result, expected)
 
 
-def test_prepare_tracks_one_segment_ok() -> None:
+def test_prepare_trackpoints_one_segment_ok() -> None:
     """Two records in same segment is ok."""
     trackpoints = [
         (dt.datetime(2020, 5, 1, 0, 0, 0, 0, dt.timezone.utc), 49.00, 8.40),
         (dt.datetime(2020, 5, 1, 0, 0, 2, 0, dt.timezone.utc), 49.01, 8.41),
     ]
-    result = processor.prepare_tracks(trackpoints)
-    expected = {
-        "dfr": pd.DataFrame(
-            {
-                "segment": [0, 0],
-                "offset": [0, 2],
-                "utm_x": [456114, 456854],
-                "utm_y": [5427629, 5428735],
-            }
-        ),
-        "utm_zone": 32,
-        "utm_ch": "U",
-    }
-    pd.testing.assert_frame_equal(result.pop("dfr"), expected.pop("dfr"))
-    assert result == expected
+    result = processor.prepare_trackpoints(trackpoints)
+    expected = pd.DataFrame(
+        {
+            "utm_x": [456114, 456854],
+            "utm_y": [5427629, 5428735],
+            "utm_zone": 32,
+            "utm_ch": "U",
+            "segment": [0, 0],
+            "offset": [0, 2],
+        }
+    )
+    pd.testing.assert_frame_equal(result, expected)
 
 
-def test_prepare_tracks_two_segments_ok() -> None:
+def test_prepare_trackpoints_two_segments_ok() -> None:
     """Two records in two segments is ok."""
     trackpoints = [
         (dt.datetime(2020, 5, 1, 0, 0, 0, 0, dt.timezone.utc), 49.00, 8.40),
         (dt.datetime(2020, 5, 1, 1, 0, 0, 0, dt.timezone.utc), 50.00, 8.40),
     ]
-    result = processor.prepare_tracks(trackpoints)
-    expected = {
-        "dfr": pd.DataFrame(
-            {
-                "segment": [0, 1],
-                "offset": [0, 0],
-                "utm_x": [456114, 456999],
-                "utm_y": [5427629, 5538803],
-            }
-        ),
-        "utm_zone": 32,
-        "utm_ch": "U",
-    }
-    pd.testing.assert_frame_equal(result.pop("dfr"), expected.pop("dfr"))
-    assert result == expected
+    result = processor.prepare_trackpoints(trackpoints)
+    expected = pd.DataFrame(
+        {
+            "utm_x": [456114, 456999],
+            "utm_y": [5427629, 5538803],
+            "utm_zone": 32,
+            "utm_ch": "U",
+            "segment": [0, 1],
+            "offset": [0, 0],
+        }
+    )
+    pd.testing.assert_frame_equal(result, expected)
 
 
-def test_prepare_tracks_too_far_away_fails() -> None:
+def test_prepare_trackpoints_too_far_away_fails() -> None:
     """Two records too far away is bad."""
     trackpoints = [
         (dt.datetime(2020, 5, 1, 0, 0, 0, 0, dt.timezone.utc), 49.00, 8.40),
         (dt.datetime(2020, 5, 1, 1, 0, 0, 0, dt.timezone.utc), 49.00, -8.40),
     ]
     with pytest.raises(processor.RegionTooLargeError):
-        processor.prepare_tracks(trackpoints)
+        processor.prepare_trackpoints(trackpoints)
 
 
 def test_prepare_waypoints_empty_raises() -> None:
@@ -95,16 +91,16 @@ def test_prepare_waypoints_two_ok() -> None:
     """Two waypoints are ok."""
     waypoints = [("station", 48.99420, 8.4003), ("castle", 49.0134, 8.4044)]
     result = processor.prepare_waypoints(waypoints)
-    expected = {
-        "dfr": pd.DataFrame(
-            {"utm_x": [456131, 456448], "utm_y": [5426984, 5429116]},
-            index=pd.Index(["station", "castle"], name="name"),
-        ),
-        "utm_zone": 32,
-        "utm_ch": "U",
-    }
-    pd.testing.assert_frame_equal(result.pop("dfr"), expected.pop("dfr"))
-    assert result == expected
+    expected = pd.DataFrame(
+        {
+            "utm_x": [456131, 456448],
+            "utm_y": [5426984, 5429116],
+            "utm_zone": 32,
+            "utm_ch": "U",
+        },
+        index=pd.Index(["station", "castle"], name="name"),
+    )
+    pd.testing.assert_frame_equal(result, expected)
 
 
 def test_prepare_waypoints_too_far_away_fails() -> None:
