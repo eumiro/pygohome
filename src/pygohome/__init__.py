e1d53d99f6449fb1ffccf8e11f71f59255211dc8
Miroslav Šedivý <6774676+eumiro@users.noreply.github.com>
2020-12-24 15:27:23 +0100

    Do not blindly copy from other projects. Bump to 0.2.1

--- src/pygohome/__init__.py
+++ src/pygohome/__init__.py
@@ -1,7 +1,7 @@
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 __title__ = "pygohome"
-__description__ = "Snowball effect in language learning"
+__description__ = "A 100% personal route optimizer in a known environment based on experience."
 __url__ = "https://github.com/eumiro/pygohome/"
 __uri__ = __url__
 __doc__ = __description__ + " <" + __uri__ + ">"
