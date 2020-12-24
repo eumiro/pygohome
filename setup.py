62ffa396b8a394ee98ff2f5bc57539dbd314980a
Miroslav Šedivý <6774676+eumiro@users.noreply.github.com>
2020-12-24 14:20:19 +0100

    replace nox with tox and remove poetry

---
+++ setup.py
@@ -0,0 +1,68 @@
+"""
+pygohome setup.py
+"""
+import re
+from pathlib import Path
+from setuptools import setup, find_packages
+
+NAME = "pygohome"
+KEYWORDS = ["gps", "navigation", "routing", "bicycle", "walk", "optimal route"]
+CLASSIFIERS = [
+    "Development Status :: 4 - Beta",
+    "Environment :: Web Environment",
+    "Framework :: Jupyter",
+    "Intended Audience :: End Users/Desktop",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: BSD License",
+    'Operating System :: OS Independent',
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3 :: Only",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Topic :: Scientific/Engineering :: GIS",
+]
+INSTALL_REQUIRES = [
+    "gpxpy",
+    "ipyleaflet",
+    "ipywidgets",
+    "networkx",
+    "notebook",
+    "numpy",
+    "pandas",
+    "scipy",
+    "utm",
+]
+
+# --+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----
+
+
+if __name__ == "__main__":
+    HERE = Path(__file__).resolve().parent
+    META_FILE = (HERE / 'src' / NAME / '__init__.py').read_text()
+    META = dict(
+        re.findall(r"^__(\w+)__ = ['\"]([^'\"]*)['\"]", META_FILE, re.M))
+    setup(
+        name=NAME,
+        description=META["description"],
+        license=META["license"],
+        url=META["url"],
+        version=META["version"],
+        author=META["author"],
+        author_email=META["email"],
+        maintainer=META["author"],
+        maintainer_email=META["email"],
+        keywords=KEYWORDS,
+        long_description=Path("README.md").read_text(),
+        long_description_content_type="text/markdown",
+        packages=find_packages(where="src"),
+        package_dir={"": "src"},
+        zip_safe=False,
+        classifiers=CLASSIFIERS,
+        install_requires=INSTALL_REQUIRES,
+        extras_require={'test': ['pytest']},
+        options={},
+        include_package_data=True,
+    )
--- src/pygohome/__init__.py
+++ src/pygohome/__init__.py
@@ -1,2 +1,13 @@
-"""pygohome."""
-__version__ = "20.5.0"
+__version__ = "0.2.0"
+
+__title__ = "pygohome"
+__description__ = "Snowball effect in language learning"
+__url__ = "https://github.com/eumiro/pygohome/"
+__uri__ = __url__
+__doc__ = __description__ + " <" + __uri__ + ">"
+
+__author__ = "Miroslav Šedivý"
+__email__ = "eumiro@gmail.com"
+
+__license__ = "BSD-3"
+__copyright__ = "Copyright (c) 2020 Miroslav Šedivý"
